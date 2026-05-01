5c308d40b98041efcff3a03a2b4940bbd2590355
JanviBhateja <bhatejajanvi@gmail.com>
2026-05-01 22:30:04 +0530

    updated ques

---
+++ oops assm 1/q5.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    cout << "Hello\nWorld" << endl;
+    cout << "C++\tProgramming" << endl;
+    cout << "ABC\bD" << endl;
+    cout << setw(10) << "Hi";
+
+    return 0;
+}
